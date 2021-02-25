de3e0c88b46b19838036e6f5a07c151724234751
TamaraBaboyan <t.baboghlian@gmail.com>
2021-02-25 23:46:24 +0400

    update buffering explanation

--- 2. Advanced_streams/1. Why_streams?/buffer.js
+++ 2. Advanced_streams/1. Why_streams?/buffer.js
@@ -2,6 +2,16 @@ var fs = require('fs');
 var http =  require('http');
 var file = '../../powder-day.mp4';
 
+/**
+ *  what we're trying to do is move data from a source to a destination. The source is the powder-day.mp4 video, and we've declared a reference 
+ * to that on line three, and the destination is going to be a HTTP response. The actual data is getting sent to the destination on line 20 
+ * when we end the response. So, we're creating a HTTP server that runs on Port 3000 and it's how we get the file that matters. 
+ * So we're using the FS.read file, which is going to read the entire contents of the MP4 video into a single variable. That variable is data,
+ * so that's our buffer, and we're sending the entire data back on line 20, once we have the entire contents of the movie loaded.
+ * 
+ * So let's take a look at what this does to memory. 
+ */
+
 http.createServer((req, res) => {
 
     fs.readFile(file, (error, data) => {
@@ -17,6 +27,7 @@ http.createServer((req, res) => {
 /**
  * I'm gonna go ahead and start the node process for buffer.js by typing 
  * $ node - -trace _gc buffer .js. 
+ * 
  * The trace gc command is gonna trace garbage collection on this process, so we can see how our garbage collector is working.
  * I'm goint to open 5 browser tabs with http://localhost:3000
  * Here we go. So we have five requests running, all getting the video. So let's take a look at what's happening.So inside of the terminal window,
